5ece69a4d1519663922b1138191324701671c54d
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2022-11-10 17:24:40 -0800

    Updates

---
+++ precache-manifest.d4332e0e8c6b8ca2b8cd6e16f129fd11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba809a789b283d528f33",
+    "url": "/Pokedex/static/css/main.88367030.chunk.css"
+  },
+  {
+    "revision": "ba809a789b283d528f33",
+    "url": "/Pokedex/static/js/main.ba809a78.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/Pokedex/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "02b8f5a0b5f1676417ee",
+    "url": "/Pokedex/static/css/2.1b196738.chunk.css"
+  },
+  {
+    "revision": "02b8f5a0b5f1676417ee",
+    "url": "/Pokedex/static/js/2.02b8f5a0.chunk.js"
+  },
+  {
+    "revision": "ba9c3bfa4614fa488a54",
+    "url": "/Pokedex/static/js/runtime~main.ba9c3bfa.js"
+  },
+  {
+    "revision": "d19e2b4de3fc13f0955923291e1527b9",
+    "url": "/Pokedex/index.html"
+  }
+];
